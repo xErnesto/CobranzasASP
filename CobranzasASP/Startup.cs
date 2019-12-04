09a77d4e914c58d2d1fc99ff5f7348a7e97ba00a
kevinbriceo567 <kevinbriceo567@gmail.com>
2019-12-04 15:45:03 -0300

    vista de deudores implementada

---
+++ CobranzasASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CobranzasASP.Startup))]
+namespace CobranzasASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
